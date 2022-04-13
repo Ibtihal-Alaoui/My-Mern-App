f9544e31c9ce4242caf3f99bbcf2b26716b7bafc
Lenovo <ibtihalhasnialaoui@gmail.com>
2022-04-13 18:30:11 +0000

    proj

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+"use strict";
+module.exports = {
+    mongoURI: 'mongodb+srv://alaoui:alaoui@cluster0.he0ol.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+};
---
+++ config/keys.ts
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI : 'mongodb+srv://alaoui:alaoui@cluster0.he0ol.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
